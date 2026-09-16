5f881bd638ba078275e2fec5b86708bd0d638b70
Nikolay Rudnev <229940054+hytorovich@users.noreply.github.com>
2026-09-16 18:16:59 +0300

    add tests

--- src/test/java/com/fintech/wallet/model/AccountTest.java
+++ src/test/java/com/fintech/wallet/model/AccountTest.java
@@ -1,4 +1,76 @@
 package com.fintech.wallet.model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AccountTest {
+
+    public void testThrows() {
+        int a = 0;
+        int b = 0;
+        int c = a / b;
+
+    }
+
+    private User nikolay;
+    private Account nikolayPay;
+
+    @BeforeEach
+    void setUp() {
+        nikolay = new User(1, 20, "Nikolay", "ogo@gmail.com");
+        nikolayPay = new Account(1, nikolay);
+    }
+
+    @Test
+    public void testBalance() {
+        nikolayPay.deposit(new BigDecimal(1000));
+        assertEquals(new BigDecimal(1000), nikolayPay.getBalance());
+    }
+
+    @Test
+    public void testWithdraw() {
+        nikolayPay.deposit(new BigDecimal(1000));
+        assertTrue(nikolayPay.withdraw(new BigDecimal(700)));
+        assertEquals(new BigDecimal(300), nikolayPay.getBalance());
+    }
+
+    @Test
+    public void withdrawReturnsFalseWhenBalanceIsInsufficient() {
+        nikolayPay.deposit(new BigDecimal(1000));
+        assertFalse(nikolayPay.withdraw(new BigDecimal(7000)));
+        assertEquals(new BigDecimal(1000), nikolayPay.getBalance());
+        assertEquals(1, nikolayPay.getTransactions().size());
+    }
+
+    @Test
+    public void throwTest() {
+        AccountTest test = new AccountTest();
+        assertThrows(ArithmeticException.class, () -> test.testThrows());
+    }
+
+    @Test
+    public void throwTest2() {
+        assertThrows(ArithmeticException.class, () -> {
+            int result = 10 / 0;
+        });
+    }
+
+    ;
+
+    @Test
+    public void negativeAmount() {
+        assertFalse(nikolayPay.withdraw(new BigDecimal("-1000")));
+        assertEquals(0, nikolayPay.getTransactions().size());
+        assertEquals(BigDecimal.ZERO, nikolayPay.getBalance());
+    }
+
+    @Test
+    public void transactionChange() {
+        nikolayPay.deposit(new BigDecimal(1000));
+        assertThrows(UnsupportedOperationException.class, () -> nikolayPay.getTransactions().clear());
+    }
 }
--- src/test/java/com/fintech/wallet/model/OptionalTest.java
+++ src/test/java/com/fintech/wallet/model/OptionalTest.java
@@ -1,4 +1,39 @@
 package com.fintech.wallet.model;
 
+import com.fintech.wallet.practice.Optional2;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class OptionalTest {
+
+    public static String[] technologies;
+    public static String target;
+
+    @BeforeEach
+    public void setup() {
+        technologies = new String[]{"Java", "Python", "JavaScript"};
+    }
+
+    @Test
+    public void testPresent() {
+        target = "Java";
+        assertTrue(Optional2.check(technologies, target).isPresent());
+        assertEquals("Java", Optional2.check(technologies, target).get());
+    }
+    @Test
+    public void testEmpty() {
+        target = "Javaa";
+        assertTrue(Optional2.check(technologies, target).isEmpty());
+    }
+
+    @Test
+    public void testThrows() {
+        target = "Javaa";
+        assertThrows(RuntimeException.class, () -> Optional2.check(technologies, target).orElseThrow());
+    }
+
 }
